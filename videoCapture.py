f546236f0a5efcb9e85d8a275693f8dffda3ad60
jinhyuk <jinhyuk.seo@gmail.com>
2017-10-27 16:56:57 +0400

    This is for the video capture. It shows the video window.

--- videoCapture.py
+++ videoCapture.py
@@ -1,3 +1,25 @@
 """ This is for the program for Video Capture. """
 
-import cv2
+import cv2, time
+
+video = cv2.VideoCapture(0)
+
+#print(video.read())
+
+while True:
+    check, frame = video.read()
+
+    #print(check)
+    print(frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #time.sleep(3)
+    cv2.imshow("VideoCapture", gray)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
